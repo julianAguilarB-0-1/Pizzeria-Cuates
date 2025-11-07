86362a6d2bdeed3300e90dd319af7d405b766262
Julián Aguilar <julian.aguilar0127@gmail.com>
2025-11-07 09:33:05 -0700

    Se corrigio la autenticacion

--- js/autenticacion.js
+++ js/autenticacion.js
@@ -57,6 +57,6 @@ btnLogin.addEventListener("click", async () => {
   } else {
     mensaje.textContent = "Bienvenido " + data.user.email;
     // Cambiar a admin
-    window.location.href = "../html/adminProducto.html";
+    window.location.href = "https://pizzeria-cuates.netlify.app/html/sesion";
   }
 });
